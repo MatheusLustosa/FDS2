8400cb014c9c48f19f2e4e486e08b6becda907a2
Pedro de Araújo Duarte Gusmão Gusmão <168600233+pedroguswander@users.noreply.github.com>
2024-11-08 20:05:34 -0300

    Update dadosPessoais.cy.js

--- cypress/e2e/dadosPessoais.cy.js
+++ cypress/e2e/dadosPessoais.cy.js
@@ -45,7 +45,7 @@ describe('Teste de dados pessoais', () => {
     beforeEach(() => {
         cy.visit('/');
         cy.get('form > :nth-child(2) > input').type('0102');
-        cy.get(':nth-child(3) > input').type('123');
+        cy.get('form > :nth-child(3) > input').type('123');
         cy.get('button').click();
         cy.get('#menu-toggle').click();
         cy.get('[href="/usuario/"]').click();
